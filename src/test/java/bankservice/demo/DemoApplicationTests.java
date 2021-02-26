b0292bb7a1666448d34e83f27055779b9e77897e
ilya-brinchuk <ilya.brinchuk@gmail.com>
2021-02-26 17:34:08 +0200

    first commit

---
+++ src/test/java/bankservice/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bankservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
